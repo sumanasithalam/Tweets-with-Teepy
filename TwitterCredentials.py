fe4d81ad0304ec72afe2301eca0639c01c261294
sumanasithalam <66976335+sumanasithalam@users.noreply.github.com>
2021-12-05 20:44:28 -0500

    Create TwitterCredentials.py

---
+++ TwitterCredentials.py
@@ -0,0 +1,6 @@
+# Template
+consumer_API_key = "insert consumer_API_key"
+consumer_API_secret_key = "insert consumer_API_secret_key"
+access_token = "insert access_token"
+access_token_secret = "insert access_token_secret"
+user_screen_name = "insert user_screen_name"
